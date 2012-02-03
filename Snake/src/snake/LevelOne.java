4d4022ee68bef728847350d8f6ed9831ba40e6d4
Ville Heikkinen <ville.heikkinen@gmail.com>
2012-02-03 13:59:23 +0200

    Lisätty collision-logiikkaa

--- Snake/src/snake/LevelOne.java
+++ Snake/src/snake/LevelOne.java
@@ -14,8 +14,8 @@ public class LevelOne {
 	
 	public LevelOne()
 	{
-		Obstacle ylareuna = new Obstacle(0, 0, 800, 20);
-		Obstacle alareuna = new Obstacle(0, 580, 800, 20);
+		Obstacle ylareuna = new Obstacle(0, 0, 810, 20);
+		Obstacle alareuna = new Obstacle(0, 590, 800, 20);
 		Obstacle vasreuna = new Obstacle(0, 20, 20, 560);
 		Obstacle oikreuna = new Obstacle(780, 20, 20, 560);
 	}
